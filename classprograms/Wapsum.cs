a289473d7d87428e06a896e68a06be0c1ca87396
syedfaizz <syedfaizz45@gmail.com>
2021-10-05 10:27:16 +0530

    [faisal] Add: welcome

---
+++ classprograms/Wapsum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class SumMatrix
+{
+    public static void Main()
+    {
+        int rows, cols;
+
+        int[,] a = {
+                          {1, 0, 1},
+                          {4, 5, 6},
+                          {1, 2, 3}
+                     };
+
+        int[,] b = {
+                          {1, 1, 1},
+                          {2, 3, 1},
+                          {1, 5, 1}
+                    };
+
+        rows = a.GetLength(0);
+        cols = a.GetLength(1);
+
+        int[,] sum = new int[rows, cols];
+
+         for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                sum[i, j] = a[i, j] + b[i, j];
+            }
+        }
+
+        Console.WriteLine("Addition of two matrices: ");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(sum[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
