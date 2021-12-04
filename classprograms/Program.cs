4d36784c1149a915b9b839bc107907db6a2905ef
Sayed Faisal <86817472+syedfaizz@users.noreply.github.com>
2021-12-04 10:30:40 +0530

    Add files via upload

--- classprograms/Program.cs
+++ classprograms/Program.cs
@@ -1,107 +1,28 @@
 ﻿using System;
-public class Queue
+namespace triangle
 {
-	private static int front, rear, capacity;
-	private static int[] queue;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;
+            int i, j;
+            for (i = 1; i <= 7; i++)
+            {
+                var result = i <= 4?k++:k--;
 
-	public Queue(int c)
-	{
-		front = rear = 0;
-		capacity = c;
-		queue = new int[capacity];
-	}
+                for (j = 1; j <= 4; j++)
+                {
+                    if (j<=k)
+                    {
 
-	public void queueEnqueue(int data)
-	{
-
-		if (capacity == rear)
-		{
-			Console.Write("\nQueue is full\n");
-			return;
-		}
-		else
-		{
-			queue[rear] = data;
-			rear++;
-		}
-		return;
-	}
-	public void queueDequeue()
-	{
-		if (front == rear)
-		{
-			Console.Write("\nQueue is empty\n");
-			return;
-		}
-		else
-		{
-			for (int i = 0; i < rear - 1; i++)
-			{
-				queue[i] = queue[i + 1];
-			}
-			if (rear < capacity)
-				queue[rear] = 0;
-
-			rear--;
-		}
-		return;
-	}
-
-	public void queueDisplay()
-	{
-		int i;
-		if (front == rear)
-		{
-			Console.Write("\nQueue is Empty\n");
-			return;
-		}
-
-		for (i = front; i < rear; i++)
-		{
-			Console.Write(" {0} <-- ", queue[i]);
-		}
-		return;
-	}
-
-	public void queueFront()
-	{
-		if (front == rear)
-		{
-			Console.Write("\nQueue is Empty\n");
-			return;
-		}
-		Console.Write("\nFront Element is: {0}", queue[front]);
-		return;
-	}
-}
-
-public class StaticQueue
-{
-
-	public static void Main(String[] args)
-	{
-
-		Queue q = new Queue(4);
-
-		q.queueDisplay();
-
-		q.queueEnqueue(20);
-		q.queueEnqueue(30);
-		q.queueEnqueue(40);
-		q.queueEnqueue(50);
-
-		q.queueDisplay();
-
-		q.queueEnqueue(60);
-
-		q.queueDisplay();
-
-		q.queueDequeue();
-		q.queueDequeue();
-		Console.Write("\n\nafter two node deletion\n\n");
-
-		q.queueDisplay();
-
-		q.queueFront();
-	}
+                        Console.Write("*");
+                    }
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
 }
